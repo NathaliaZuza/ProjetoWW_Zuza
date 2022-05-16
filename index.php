771b88ce0959dd950d9a441c11c78d93424793ec
Wendel-D <wendeldaniel263@gmail.com>
2022-05-16 08:45:09 -0300

    nome

--- index.php
+++ index.php
@@ -112,7 +112,7 @@
             </p>
         </div> -->
         <div class="criacao">
-            <p id="criado">Criado por: <span id="autores">Nathália Zuza, Weskley Borges e Wendel Cardoso</span></p>
+            <p id="criado">Criado por: <span id="autores">Nathália Zuza, Weskley Borges e Wendel Barbosa</span></p>
         </div>
     </footer>
 </html>
