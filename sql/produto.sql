908b3eedcc25016ad6f17f79239b779ff47c11b9
NathaliaZuza <nathalia555zuza@gmail.com>
2022-05-16 21:12:19 -0300

    atualização banco

--- sql/produto.sql
+++ sql/produto.sql
@@ -38,7 +38,11 @@ CREATE TABLE IF NOT EXISTS `ww_zuza`.`produto` (
   PRIMARY KEY (`id`),
   FOREIGN KEY(categoria_id) REFERENCES categoria(id)
 );
-INSERT INTO `produto` (`nome`, `preco`, `cores`, `material`, `tamanho`, `prazo`, `qtd`, `foto`, `categoria_id`) VALUES
-('Panfleto 4x0', '70,00', '4x0', 'Papel Couchê', '15cm x 10cm', '3 dias úteis', '1000un', '627bfb295a9a0.png', 1),
-('Panfleto 4x4', '200,00', '4x4', 'Papel Couchê', '15cm x 10cm', '2 dias úteis', '5000un', '627bffc456c7e.png', 1),
-('Panfleto 20x10', '450,00', '4x4', 'Papel Couchê', '20cm x 10cm', '3 dias úteis', '10000un', '627c035424952.png', 1);
+
+INSERT INTO `produto` (`id`, `nome`, `preco`, `cores`, `material`, `tamanho`, `prazo`, `qtd`, `foto`, `categoria_id`) VALUES
+(1, 'Panfleto 4x0', '70,00', '4x0 (color', 'Papel Offset  90g', '15cm x 10cm', '3 dias úteis', '1000un', '627bfb295a9a0.png', 1),
+(2, 'Panfleto 4x4', '200,00', '4x4', 'Papel Couchê', '15cm x 10cm', '2 dias úteis', '5000un', '627bffc456c7e.png', 1),
+(3, 'Panfleto Ofício', '450,00', '4x4', 'Papel Offset  90g', '20cm x 10cm', '3 dias úteis', '10000un', '627c035424952.png', 1),
+(6, 'Cartão de Visita', '80,00', '4x0', 'Papel Couchê 300g', '9cm x 5cm', '3 dias úteis', '1000un', '6282c299b49ff.png', 2),
+(8, 'Cartão de Visita 4x4', '200,00', '4x4', 'Papel Couchê 300g', '9cm x 5cm', '3 dias úteis', '1000un', '6282c3a45923e.png', 2),
+(10, 'Cartão de Visita 4x0', '60,00', '4x0', 'Papel Couchê 300g', '9cm x 5cm', '2 dias úteis', '1000un', '6282c4124e65a.png', 2);
