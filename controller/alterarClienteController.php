ff48f0fbabb8263bfb6bea9e455fee768c7fb70f
NathaliaZuza <nathalia555zuza@gmail.com>
2022-05-17 17:19:52 -0300

    comentei tudo que agora é da tabela endereço

--- controller/alterarClienteController.php
+++ controller/alterarClienteController.php
@@ -5,24 +5,24 @@ require_once '../dao/ClienteDAO.php';
 $id = $_POST["id"];
 $nome = $_POST["nome"];
 $cpf = $_POST["cpf"];
-$telefone = $_POST["telefone"];
-$cep = $_POST["cep"];
-$estado = $_POST["estado"];
-$cidade = $_POST["cidade"];
-$endereco = $_POST["endereco"];
-$numero_casa = $_POST["numero_casa"];
+// $telefone = $_POST["telefone"];
+// $cep = $_POST["cep"];
+// $estado = $_POST["estado"];
+// $cidade = $_POST["cidade"];
+// $endereco = $_POST["endereco"];
+// $numero_casa = $_POST["numero_casa"];
 
 
 $clienteDTO = new ClienteDTO();
 $clienteDTO->setId( $id );
 $clienteDTO->setNome( $nome );
 $clienteDTO->setCpf( $cpf );
-$clienteDTO->setTelefone( $telefone);
-$clienteDTO->setCep($cep);
-$clienteDTO->setEstado($estado);
-$clienteDTO->setCidade($cidade);
-$clienteDTO->setEndereco($endereco);
-$clienteDTO->setNumero_casa($numero_casa);
+// $clienteDTO->setTelefone( $telefone);
+// $clienteDTO->setCep($cep);
+// $clienteDTO->setEstado($estado);
+// $clienteDTO->setCidade($cidade);
+// $clienteDTO->setEndereco($endereco);
+// $clienteDTO->setNumero_casa($numero_casa);
 
 
 
