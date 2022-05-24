26437e296dda2adc661f34edd2e418d9fea0713e
NathaliaZuza <nathalia555zuza@gmail.com>
2022-05-24 18:58:06 -0300

    testes

---
+++ testegrid.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="styleSheet" href="./css/carrinho.css">
+    <link rel="stylesheet" href="../css/headerMenu.css">
+</head>
+<body>
+<?php include './view/headerMenu.php' ?>
+    <section class="container">
+        <div class="endereco">conteudoaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+            <p>a</p>
+            <p>a</p>
+            <p>a</p>
+            <p>a</p>
+            <p>a</p>
+            <p>a</p>
+        
+
+        </div>
+        <div class="resumo"> <p>a</p>
+            <p>a</p>
+            <p>a</p>
+            <p>a</p></div>
+        <div class="item1"> <p>a</p>
+            <p>a</p>
+            <p>a</p>
+            <p>a</p></div>
+        <div class="item2"> <p>a</p>
+            <p>a</p>
+            <p>a</p>
+            <p>a</p></div>
+    </section>
+
+    <footer>
+        <div class="social">
+            <p><span class="logo">WW.ZUZA</span></p>
+            <div class="socialcirc">
+                <a href="#" class="fab fa-facebook-f"></a>
+                <a href="#" class="fab fa-twitter"></a>
+                <a href="#" class="fab fa-instagram"></a>
+                <a href="#" class="fab fa-whatsapp"></a>
+                <p>Copyright © 2022 Todos os <br> direitos reservados</p>
+            </div>
+        </div>
+      <!--   <div class="pagamento">
+            <p>Formas de pagamento:</p>
+            <p>
+                <img src="./img/pagamento-07.png" width="70x">
+                <img src="./img/pagamento-08.png" width="70px">
+            </p>
+        </div> -->
+        <div class="criacao">
+            <p id="criado">Criado por: <span id="autores">Nathália Zuza, Weskley Borges e Wendel Barbosa</span></p>
+        </div>
+    </footer>
+</body>
+</html>
