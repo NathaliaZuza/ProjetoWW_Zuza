3c8fad5b8b469dc44ff2441ccafc57f02743d355
NathaliaZuza <nathalia555zuza@gmail.com>
2022-05-25 13:28:52 -0300

    limpa js

--- sql/ww_Zuzaatualizado.sql
+++ sql/ww_Zuzaatualizado.sql
@@ -121,6 +121,15 @@ CREATE TABLE IF NOT EXISTS `ww_zuza`.`produto` (
 ENGINE = InnoDB
 AUTO_INCREMENT = 12
 DEFAULT CHARACTER SET = utf8mb4;
+INSERT INTO `produto` (`id`, `nome`, `descricao`, `preco`, `cores`, `material`, `tamanho`, `prazo`, `qtd`, `foto`, `categoria_id`) VALUES
+(1, 'Panfleto 4x0', 'teste', '70', '4x0 (colorido)', 'Papel Offset  90g', '15cm x 10cm', '3 dias úteis', '1000un', '627bfb295a9a0.png', 1),
+(2, 'Panfleto 4x4', 'teste3','200', '4x4', 'Papel Couchê', '15cm x 10cm', '2 dias úteis', '5000un', '627bffc456c7e.png', 1),
+(3, 'Panfleto Ofício', 'teste2', '450', '4x4', 'Papel Offset  90g', '20cm x 10cm', '3 dias úteis', '10000un', '627c035424952.png', 1),
+(6, 'Cartão de Visita', 'teste3', '80', '4x0', 'Papel Couchê 300g', '9cm x 5cm', '3 dias úteis', '1000un', '6282c299b49ff.png', 2),
+(8, 'Cartão de Visita 4x4', 'teste4', '200', '4x4', 'Papel Couchê 300g', '9cm x 5cm', '3 dias úteis', '1000un', '6282c3a45923e.png', 2),
+(10, 'Cartão de Visita 4x0', 'teste5', '60', '4x0', 'Papel Couchê 300g', '9cm x 5cm', '2 dias úteis', '1000un', '6282c4124e65a.png', 2),
+(11, 'Cardápio', 'teste6', '250', '4x4 (colorido)', 'Papel Couchê 95g', '20cm x 10cm', '4 dias úteis', '30un', '6285b5b2ed663.png', 3);
+
 
 
 -- -----------------------------------------------------
