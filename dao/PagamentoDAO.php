0b232170cbd82e1541ff672b4ae43558185b71bf
Wendel-D <wendeldaniel263@gmail.com>
2022-05-25 09:03:04 -0300

    tentativa

---
+++ dao/PagamentoDAO.php
@@ -0,0 +1,31 @@
+<?php
+
+require_once 'conexao/Conexao.php';
+
+class PagamentoDAO {
+    private $pdo;
+
+    public function __construct() {
+        $this->pdo = Conexao::getInstance();
+    }
+
+    public function salvar( PagamentoDTO $pagamentoDTO ) {
+        try {
+            $sql = "INSERT INTO pagamento "
+                . "(n_cartao, nome_cartao, validade, verificacao, cpf, data_nasc, parcelamento, pedido_id "
+                . "VALUES(?,?,?,?,?,?,?,?)";
+
+            $stmt = $this->pdo->prepare( $sql );
+            $stmt->bindValue( 1, $pagamentoDTO->getN_cartao() );
+            $stmt->bindValue( 2, $pagamentoDTO->getNome_cartao() );
+            $stmt->bindValue( 3, $pagamentoDTO->getValidade() );
+            $stmt->bindValue( 4, $pagamentoDTO->getVerificacao() );
+            $stmt->bindValue( 5, $pagamentoDTO->getCpf() );
+            $stmt->bindValue( 6, $pagamentoDTO->getData_nasc() );
+            $stmt->bindValue( 7, $pagamentoDTO->getParcelamento() );
+            $stmt->bindValue( 8, $pagamentoDTO->getPedido_id() );
+            return $stmt->execute();
+        } catch ( PDOException $e ) {
+            echo $e->getMessage();
+        }
+    }}
---
+++ dto/PagamentoDTO.php
@@ -0,0 +1,69 @@
+<?php
+
+class PagamentoDTO {
+
+    private $id;
+    private $n_cartao;
+    private $nome_cartao;
+    private $validade;
+    private $verificacao;
+    private $cpf;
+    private $data_nasc;
+    private $parcelamento;
+    private $pedido_id;
+
+    public function getId(){
+        return $this->id;
+    }
+    public function setId($id){
+        $this->id = $id;
+    }
+    public function getN_cartao(){
+        return $this->n_cartao;
+    }
+    public function setN_cartao($n_cartao){
+        $this->n_cartao = $n_cartao;
+    }
+    public function getNome_cartao(){
+        return $this->nome_cartao;
+    }
+    public function setNome_cartao($nome_cartao){
+        $this->nome_cartao = $nome_cartao;
+    }
+    public function getValidade(){
+        return $this->validade;
+    }
+    public function setValidade($validade){
+        $this->validade = $validade;
+    }
+    public function getVerificacao(){
+        return $this->verificacao;
+    }
+    public function setVerificacao($verificacao){
+        $this->verificacao = $verificacao;
+    }
+    public function getCpf(){
+        return $this->cpf;
+    }
+    public function setCpf($cpf){
+        $this->cpf = $cpf;
+    }
+    public function getData_nasc(){
+        return $this->data_nasc;
+    }
+    public function setData_nasc($data_nasc){
+        $this->data_nasc = $data_nasc;
+    }
+    public function getParcelamento(){
+        return $this->parcelamento;
+    }
+    public function setParcelamento($parcelamento){
+        $this->parcelamento = $parcelamento;
+    }
+    public function getPedido_id(){
+        return $this->pedido_id;
+    }
+    public function setPedido_id($pedido_id){
+        $this->pedido_id = $pedido_id;
+    }
+}
