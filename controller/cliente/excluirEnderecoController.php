8fa53ae2a55e7120fe6fa26206434cc20117de7c
NathaliaZuza <nathalia555zuza@gmail.com>
2022-05-30 11:48:29 -0300

    cliente

--- controller/cliente/excluirEnderecoController.php
+++ controller/cliente/excluirEnderecoController.php
@@ -2,6 +2,7 @@
 require_once '../../dao/enderecoDAO.php';
 $idCliente = $_GET['excluirId'];
 $enderecoDAO = new EnderecoDAO();
+
 if ( $enderecoDAO->deleteById( $idCliente ) ) {
-     header( "Location: ../view/minhaConta.php" );
+     header( "Location: ../../view/cliente/minhaConta.php" );
 }
