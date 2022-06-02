c64e1c8f87592a4687c1a85b91bd28d628d35796
NathaliaZuza <nathalia555zuza@gmail.com>
2022-06-01 23:31:40 -0300

    lista pedidos

--- view/cliente/compras.php
+++ view/cliente/compras.php
@@ -4,82 +4,79 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="/css/cadastro.css">
+    <link rel="stylesheet" href="/css/headerMenu.css">
+    <link rel="stylesheet" href="/css/minhaConta.css">
+    <link rel="stylesheet" href="/css/listaProdutosCliente.css">
+    <title>Meus pedidos | WWZUZA</title>
 </head>
 <body>
-    <h1>Listar compras</h1>
 
-    <?php
-       require_once '../../dao/ProdutoDAO.php';
-       require_once '../../dao/CarrinhoDAO.php';
-       require_once '../../dao/ClienteDAO.php';
-       session_start();
-       $idCliente = $_SESSION["idCliente"];
-    $produtoDAO = new ProdutoDAO();  
-    $carrinhoDAO = new CarrinhoDAO();
-    $clienteDAO = new ClienteDAO();
-    $carrinhos = $carrinhoDAO->findById($idCliente);
-    $pedidos = $carrinhoDAO->findAllPedidos();
-    
 
-    echo "<main class='container2'>";
-    echo "<div class='conteudo2'>";
-      
+<?php
+   require_once '../../dao/ProdutoDAO.php';
+   require_once '../../dao/CarrinhoDAO.php';
+   require_once '../../dao/ClienteDAO.php';
+   session_start();
+   $idCliente = $_SESSION["idCliente"];
+$produtoDAO = new ProdutoDAO();  
+$carrinhoDAO = new CarrinhoDAO();
+$clienteDAO = new ClienteDAO();
+$carrinhos = $carrinhoDAO->findAll();
+$pedidos = $carrinhoDAO->findAllPedidos();
 
-   foreach ($carrinhos as $carrinho){  
+
+echo "<main class='container'>";
+echo "<div class='conteudo'>";
+  echo "<h1>Meus pedidos</h1>";
+foreach ($carrinhos as $carrinho){
+    if($carrinho['cliente_id'] == $idCliente){
         echo "<table border='1'>";
         // echo "<tr><th>Cliente</th>"; 
         // echo "<th>Valor</th>";
         // echo "<th>Produtos</th>"; 
         // echo "<th>Foto</th>";    
         // echo "<th>Data</th>";
-
+        
         $cliente = $clienteDAO->findById($idCliente);      
-        echo "<tr><th>Cliente</th>";
-        echo "<td class='nome-lista'>{$cliente["nome"]}</td></tr>";     
         //echo "<td class='nome-lista'>";
-     
-            foreach($pedidos as $pedido){
-                if($pedido['pedido_id'] == $carrinho['id']){
-                    echo "<tr><th>Valor</th>";
-                    echo "<td> {$pedido['preco']}</td></tr>";
-                    $produto = $produtoDAO->findById($pedido['produto_id']);
-                    echo "<tr><th>Produto:</th>"; 
-                    echo "<td> {$produto['nome']} </td>";
-                    echo "<tr><th>Quantidade</th>"; 
-                    echo "<td> {$pedido['quantidade']} </td></tr>";
-                    echo "<td><div class='img_produto'>";
-                    echo "<p> <img src='/img/produto/foto/{$produto['foto']}' width='112' /></p>";
-                    echo "</div>";
-                    echo "</td>";
-                }
-            }    
-    ?>   
-        <!-- <td><div class='img_produto'>
-                <p> <img src="/img/produto/foto/<?php echo $produto["foto"] ?>" width="112" /></p>
-            </div>
-        </td>
-        </tr> -->
-    <?php
+    
+        foreach($pedidos as $pedido){
+            if($pedido['pedido_id'] == $carrinho['id']){
+               
+                $produto = $produtoDAO->findById($pedido['produto_id']);
+                echo "<tr><th>Produto:</th>"; 
+                echo "<td> {$produto['nome']} </td>";
+                echo "<td><img src='../../img/produto/foto/{$produto['foto']}' width='112' /></tr>"; 
+                echo "<tr><th>Valor</th>";
+                echo "<td>" , number_format(($pedido["preco"]), 2, ",", "." ), "</td></tr>";
+                echo "<tr><th>Quantidade</th>"; 
+                echo "<td> {$pedido['quantidade']} </td></tr>";
+                echo "</td>";
+            }
+        }
+    
+
+?>   
+    <!-- <td><div class='img_produto'>
+            <p> <img src="/img/produto/foto/<?php echo $produto["foto"] ?>" width="112" /></p>
+        </div>
+    </td>
+    </tr> -->
+<?php
         echo "<tr>";
         echo "<th>Data</th>";
         echo "<td class='nome-lista'>{$carrinho["data"]}</td>"; 
         echo "</tr>";
         echo "<tr>";
         echo "<th>Valor total:</th>";
-        echo "<td class='nome-lista'>{$carrinho["valor_total"]}</td>"; 
+        echo "<td class='nome-lista'>" , number_format(($carrinho["valor_total"]), 2, ",", "." ), "</td>"; 
         echo "</tr>";
         echo "</main>";
         echo "</div>";
-   }
-    ?>
-
-    <script>
-          function confirmarExcluir(){
-            return confirm("Você está apagando um produto do catálogo. Tem certeza que deseja excluir?");
-        } 
-    </script>
-
+    }
+}
+?>
 
 </body>
 </html>
